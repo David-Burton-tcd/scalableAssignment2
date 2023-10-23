e8e4870fd1ee8036d9d1e6d5d56f017a66428633
David Burton <davidburton@Davids-MacBook-Air.local>
2023-10-23 12:09:15 +0100

    Initial commit

---
+++ generate.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import os
+import numpy
+import random
+import string
+import cv2
+import argparse
+import captcha.image
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--width', help='Width of captcha image', type=int)
+    parser.add_argument('--height', help='Height of captcha image', type=int)
+    parser.add_argument('--max-length', help='Length of captchas in characters', type=int)
+    parser.add_argument('--count', help='How many captchas to generate', type=int)
+    parser.add_argument('--output-dir', help='Where to store the generated captchas', type=str)
+    parser.add_argument('--symbols', help='File with the symbols to use in captchas', type=str)
+    args = parser.parse_args()
+
+    if args.width is None:
+        print("Please specify the captcha image width")
+        exit(1)
+
+    if args.height is None:
+        print("Please specify the captcha image height")
+        exit(1)
+
+    if args.max_length is None:
+        print("Please specify the max captcha length")
+        exit(1)
+
+    if args.count is None:
+        print("Please specify the captcha count to generate")
+        exit(1)
+
+    if args.output_dir is None:
+        print("Please specify the captcha output directory")
+        exit(1)
+
+    if args.symbols is None:
+        print("Please specify the captcha symbols file")
+        exit(1)
+
+    jjesterFont = 'jjester.otf'
+    captcha_generator = captcha.image.ImageCaptcha(width=args.width, height=args.height, fonts=[jjesterFont])
+
+    symbols_file = open(args.symbols, 'r')
+    captcha_symbols = symbols_file.readline().strip()
+    symbols_file.close()
+
+    print("Generating captchas with symbol set {" + captcha_symbols + "}")
+
+    if not os.path.exists(args.output_dir):
+        print("Creating output directory " + args.output_dir)
+        os.makedirs(args.output_dir)
+
+    for i in range(args.count):
+
+        random_str = ''.join([random.choice(captcha_symbols) for j in range(random.randint(1, args.max_length))])
+        image_path = os.path.join(args.output_dir, random_str+'.png')
+        
+        if os.path.exists(image_path):
+            print('Skipping duplicate image')
+            # version = 1
+            # while os.path.exists(os.path.join(args.output_dir, random_str + '_' + str(version) + '.png')):
+            #     version += 1
+            # image_path = os.path.join(args.output_dir, random_str + '_' + str(version) + '.png')
+        else:
+            image = numpy.array(captcha_generator.generate_image(random_str))
+            cv2.imwrite(image_path, image)
+
+if __name__ == '__main__':
+    main()
